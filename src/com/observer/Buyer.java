caf95b0067fa1b5bac31768ea6c4937d70396298
kirill-predkel-edu <86263425+kirill-predkel-edu@users.noreply.github.com>
2021-06-21 22:45:01 +0300

    Delete src/com directory

--- src/com/observer/Buyer.java
+++
@@ -1,9 +0,0 @@
-package com.observer;
-
-public class Buyer implements Subscriber {
-
-    @Override
-    public void takeCake() {
-        System.out.println("Wow! Price drops! I need to buy the cake");
-    }
-}
--- src/com/observer/Observable.java
+++
@@ -1,7 +0,0 @@
-package com.observer;
-
-public interface Observable {
-    void addSubscriber(Subscriber s);
-    void removeSubscriber(Subscriber s);
-    void notifySubscriber();
-}
